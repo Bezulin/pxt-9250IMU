85c3b510403575a42f288f1171e3abf448a0aecb
Bezulin <53722790+Bezulin@users.noreply.github.com>
2019-08-05 08:04:29 -0700

    Updates.

--- main.ts
+++ main.ts
@@ -1,20 +1,22 @@
 enum Axis {
     //% block="x axis"
-    x=67,
+    x = 67,
     //% block="y axis"
-    y=69,
+    y = 69,
     //% block="z axis"
-    z=71
+    z = 71
 }
 
 //% color="#AA11FF"
-namespace IMU{
+namespace IMU {
     /**
      * reads data from a MPU-9250 IMU
      */
     //% block
-    export function readGyro(axis:Axis):number{
-        pins.i2cWriteNumber(104, axis, NumberFormat.Int16BE,true),
-        return(pins.i2cReadBuffer(104, 2))
+    export function readGyro(axis: Axis): number {
+        let data = 0
+        pins.i2cWriteNumber(104, axis, NumberFormat.Int16BE, true)
+        data = pins.i2cReadBuffer(104, 2)
+        return(data)
     }
 }
