2832d686849f1544d2d519997c6a1821c233583c
Bezulin <53722790+Bezulin@users.noreply.github.com>
2019-08-05 06:52:24 -0700

    Updates.

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+enum Axis {
+    //% block = "x axis"
+    x = 0
+    //% block = "y axis"
+    y=2
+    //% block = "z axis"
+    z=4
+}
+
 namespace IMU{
     /**
      * reads data from a MPU-9250 IMU
