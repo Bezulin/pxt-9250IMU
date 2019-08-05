8ba2f0f18825d5ef5f425952d3c1f9d506f46c48
Bezulin <53722790+Bezulin@users.noreply.github.com>
2019-08-05 06:35:59 -0700

    Just getting started

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+namespace IMU{
+    /**
+     * reads data from a MPU-9250 IMU
+     */
+    export function readGyro(axis:Axis):void{
+
+    }
+}
