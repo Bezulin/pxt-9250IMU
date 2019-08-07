6e45a5d380382b5be62398dfd579c270fc542bdf
Bezulin <53722790+Bezulin@users.noreply.github.com>
2019-08-07 12:38:20 -0700

    Updates.

--- main.ts
+++ main.ts
@@ -44,7 +44,7 @@ enum Gyrange {
 }
 
 //% color="#2244FF"
-//% groups=['readings','setup','advanced']
+//% groups=['readings','setup','others','advanced']
 namespace IMU9250 {
     // initialize globals
     let xcal = 0    //gyro calibration
